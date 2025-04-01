d492077c99428bc484051a332f20f4c411c5a50a
Sowa <123586667+Junasz121@users.noreply.github.com>
2025-04-01 15:24:07 +0200

    Update MainWindow.xaml.cs

--- FunkctionGraphDrawer/FunkctionGraphDrawer/MainWindow.xaml.cs
+++ FunkctionGraphDrawer/FunkctionGraphDrawer/MainWindow.xaml.cs
@@ -14,10 +14,7 @@ using System.Text.RegularExpressions;
 
 namespace FunkctionGraphDrawer;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-/// 
+
 
 
 public partial class MainWindow : Window
